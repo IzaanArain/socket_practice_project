2acb9f78c4352da514a66c578b9af5a001a5baa4
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-11-06 20:28:35 +0500

    updated

--- socket_practice_backend/config/DBConnection.js
+++ socket_practice_backend/config/DBConnection.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 const Connect=async()=>{
     try{
         const conn=await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log(`Connected to ${conn.connection.host}/${conn.connection.name}`)
+        console.log(`Connected to ${conn.connection.host}/${conn.connection.name}`.cyan)
     }catch(err){
         console.error("Error",err.message.red);
         process.exit(1);
--- socket_practice_backend/server.js
+++ socket_practice_backend/server.js
@@ -7,6 +7,7 @@ const colors = require("colors");
 const { Server } = require("socket.io");
 const Connect = require("./config/DBConnection");
 const Message = require("./model/MessageModel");
+const { Socket } = require("dgram");
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
@@ -19,21 +20,28 @@ const io = new Server(server, {
 // const io=new Server(server);
 
 io.on("connection", (socket) => {
-  // console.log("user connected", socket.id);
-  socket.join("room");
+  console.log("user connected", socket.id);
+  // socket.join("room");
 
-  // socket.on("join_room", (data,callback) => {
-  //   socket.join(data);
-  // });
-  
+  socket.on("join_room", (data) => {
+    console.log(data)
+    socket.join(data.room);
+    console.log(socket.rooms)
+  });
+
   socket.on("send_message", (data) => {
     console.log("sending", data);
     socket.to(data.room).emit("receive_message", data);
+    console.log(socket.rooms)
   });
+  
+  // socket.on("receive_message",(data)=>{
+  //   console.log("received",data)
+  // })
 
-  socket.on("receive_message",(data)=>{
-    console.log("received",data)
-  })
+   // socket.on("message", (data) => {
+  //   socket.emit("message", data);
+  // });
 
   socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
@@ -44,7 +52,7 @@ const PORT = process.env.PORT || 3000;
 
 Connect().then(() => {
   server.listen(PORT, () => {
-    console.log(`Server runnimg on http://localhost:${PORT}`);
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`.cyan);
+    console.log(`Server running on port ${PORT}`.cyan);
   });
 });
